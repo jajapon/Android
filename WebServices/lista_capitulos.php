4545a70d1af5dd2c551a88d23c3801802102136e
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-09-11 03:20:23 +0200

    importacion de webservices

---
+++ WebServices/lista_capitulos.php
@@ -0,0 +1,45 @@
+<?php
+if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+  $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+  $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+  $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+  $db_name=$database; //Openshift db name
+} else {
+  if($username==null){
+    header("Location: ./installation/index.php");
+  }else{
+    $db_user='japon'; //my db user
+    $db_host='localhost'; //my db host
+    $db_password='1234'; //my db password
+    $db_name='animejapan'; //my db name
+  }
+}
+ $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+      //TESTING IF THE CONNECTION WAS RIGHT
+   if ($connection->connect_errno) {
+        printf("Connection failed: %s\n", $connection->connect_error);
+        exit();
+
+   }else{
+   }
+   $jsonArray = array();
+
+   $query = 'SELECT * FROM capitulo WHERE animeid = '.$_GET["animeid"];
+   if($result = $connection->query($query)){
+     if ($result->num_rows==0) {
+
+     }else{
+         while($obj=$result->fetch_object()){
+            array_push($jsonArray,$obj);
+         }
+         //var_dump($jsonArray);
+
+        //devuelvo los datos en forma de JSON
+        $final_res =json_encode($jsonArray) ;
+        echo $final_res;
+    }
+   }else{
+      echo $connection->error;
+   }
+?>
