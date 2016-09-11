1096c79ec68eb242d5e25e2b236669fffb4b0435
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-09-11 03:25:50 +0200

    ultimo cambio

--- WebServices/lista_animes.php
+++ WebServices/lista_animes.php
@@ -3,16 +3,12 @@
      $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
      $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
      $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-     $db_name=$database; //Openshift db name
+     $db_name="animejapan"; //Openshift db name
    } else {
-     if($username==null){
-       header("Location: ./installation/index.php");
-     }else{
        $db_user='japon'; //my db user
        $db_host='localhost'; //my db host
        $db_password='1234'; //my db password
        $db_name='animejapan'; //my db name
-     }
    }
    $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
       //TESTING IF THE CONNECTION WAS RIGHT
