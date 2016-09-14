a2cc68ea4cf18a072b3cf9711d8e784ebcc95b64
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-09-14 17:04:11 +0200

    ultimo cambio

--- WebServices/index.php
+++ WebServices/index.php
@@ -130,17 +130,19 @@
        ?>
 
        <?php
-          $fichero = 'animejapan.apk';
-              if (file_exists($fichero)) {
-              header('Content-Description: File Transfer');
-              header('Content-Type: application/octet-stream');
-              header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
-              header('Expires: 0');
-              header('Cache-Control: must-revalidate');
-              header('Pragma: public');
-              header('Content-Length: ' . filesize($fichero));
-              readfile($fichero);
-              exit;
+          if(isset($_GET["descarga"])){
+            $fichero = 'animejapan.apk';
+                if (file_exists($fichero)) {
+                header('Content-Description: File Transfer');
+                header('Content-Type: application/octet-stream');
+                header('Content-Disposition: attachment; filename="'.basename($fichero).'"');
+                header('Expires: 0');
+                header('Cache-Control: must-revalidate');
+                header('Pragma: public');
+                header('Content-Length: ' . filesize($fichero));
+                readfile($fichero);
+                exit;
+            }
           }
         ?>
     </table>
