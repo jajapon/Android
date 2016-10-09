507301b540e7e98b197aa06f3ffad58a06749566
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-10-09 15:43:06 +0200

    cambios
    cambios

--- WebServices/alta_usuario.php
+++ WebServices/alta_usuario.php
@@ -1,26 +1,33 @@
 <?php
-   $username='japon';
-   $password='1234';
-   $database='android';
-   $localhost='localhost';
+    if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+      $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+      $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+      $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+      $db_name="animejapan"; //Openshift db name
+    } else {
+        $db_user='japon'; //my db user
+        $db_host='localhost'; //my db host
+        $db_password='1234'; //my db password
+        $db_name='animejapan'; //my db name
+    }
+    $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+       //TESTING IF THE CONNECTION WAS RIGHT
+    if ($connection->connect_errno) {
+         printf("Connection failed: %s\n", $connection->connect_error);
+         exit();
 
-   $connection = new mysqli($localhost, $username, $password, $database);
-      //TESTING IF THE CONNECTION WAS RIGHT
-   if ($connection->connect_errno) {
-        printf("Connection failed: %s\n", $connection->connect_error);
-        exit();
-
-   }else{
-   }
+    }else{
+    }
 
    $username = $_GET["username"];
    $userpass = $_GET["userpass"];
    $email = $_GET["email"];
    //$query = 'INSERT INTO USUARIO VALUES("prueba","1234","s@gmail.com");';
-   $query = 'INSERT INTO USUARIO VALUES("'.$username.'","'.$userpass.'","'.$email.'");';
+   $query = 'INSERT INTO usuario VALUES("'.$username.'","'.$userpass.'","'.$email.'");';
    if($connection->query($query)){
 
    }else{
-      echo $connection->error;
+      echo $connection->erro;
+
    }
 ?>
--- WebServices/login.php
+++ WebServices/login.php
@@ -1,10 +1,16 @@
 <?php
-    $username='japon';
-    $password='1234';
-    $database='android';
-    $localhost='localhost';
-
-    $connection = new mysqli($localhost, $username, $password, $database);
+    if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+      $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+      $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+      $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+      $db_name="animejapan"; //Openshift db name
+    } else {
+        $db_user='japon'; //my db user
+        $db_host='localhost'; //my db host
+        $db_password='1234'; //my db password
+        $db_name='animejapan'; //my db name
+    }
+    $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
        //TESTING IF THE CONNECTION WAS RIGHT
     if ($connection->connect_errno) {
          printf("Connection failed: %s\n", $connection->connect_error);
@@ -13,7 +19,7 @@
     }else{
     }
 
-    $query = "SELECT  * FROM USUARIO WHERE USERNAME = '".$_GET["username"]."' AND USERPASS = '".$_GET["userpass"]."'";
+    $query = "SELECT  * FROM usuario  WHERE username = '".$_GET["username"]."' AND userpass = '".$_GET["userpass"]."'";
     if($result = $connection->query($query)){
       if($result->num_rows == 0){
         echo "Login incorrecto";
