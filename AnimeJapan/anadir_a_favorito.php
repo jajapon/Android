2ccfa2483441d46b7a631e9454cbecddffe7b106
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-10-10 15:37:47 +0200

    animejapan favs
    favs

---
+++ AnimeJapan/anadir_a_favorito.php
@@ -0,0 +1,41 @@
+<?php
+    if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+      $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+      $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+      $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+      $db_name="animejapan"; //Openshift db name
+    } else {
+        $db_user='japon'; //my db user
+        $db_host='localhost'; //my db host
+        $db_password='1234'; //my db password
+        $db_name='animejapan'; //my db name
+    }
+    $connection = new mysqli($db_host, $db_user, $db_password, $db_name);
+       //TESTING IF THE CONNECTION WAS RIGHT
+    if ($connection->connect_errno) {
+         printf("Connection failed: %s\n", $connection->connect_error);
+         exit();
+
+    }else{
+    }
+
+   $username = $_GET["username"];
+   $animeid = $_GET["animeid"];
+   //$query = 'INSERT INTO USUARIO VALUES("prueba","1234","s@gmail.com");';
+   $query = 'SELECT * FROM listaanimesfavoritos WHERE username="'.$username.'" AND animeid='.$animeid.'';
+   if ($result = $connection->query($consulta)) {
+     if ($result->num_rows==0) {
+       $query = 'INSERT INTO listaanimesfavoritos VALUES("'.$username.'",'.$animeid.');';
+       if($connection->query($query)){
+          echo "el anime fue añadido a tu lista de favoritos";
+       }else{
+          echo "$connection->erro";
+       }
+     } else {
+       echo "El anime fue añadido a favoritos anteriormente";
+     }
+   }else{
+     echo "$connection->erro";
+   }
+
+?>
