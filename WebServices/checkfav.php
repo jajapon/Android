f915303059daa8522c52e806c83b2ec7d81f3247
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2016-10-11 22:08:17 +0200

    B
    B

--- WebServices/checkfav.php
+++ WebServices/checkfav.php
@@ -1,8 +1,15 @@
 <?php
-   $username='japon';
-   $password='1234';
-   $database='android';
-   $localhost='localhost';
+if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+  $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+  $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+  $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+  $db_name="animejapan"; //Openshift db name
+} else {
+    $db_user='japon'; //my db user
+    $db_host='localhost'; //my db host
+    $db_password='1234'; //my db password
+    $db_name='animejapan'; //my db name
+}
 
    $connection = new mysqli($localhost, $username, $password, $database);
       //TESTING IF THE CONNECTION WAS RIGHT
