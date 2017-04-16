05d13fdd2900a9ecba3791e1f47d0338883d16bc
Juan Antonio Japon de la Torre <juan.antonio.japon@gmail.com>
2017-04-16 23:25:31 +0200

    Conexion modificada
    modificacion

--- AnimeJapan_v2/php/conexion.php
+++ AnimeJapan_v2/php/conexion.php
@@ -3,7 +3,7 @@
       $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
       $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
       $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-      $db_name="animejapan"; //Openshift db name
+      $db_name="new_animejapan"; //Openshift db name
     } else {
         $db_user='japon'; //my db user
         $db_host='localhost'; //my db host
